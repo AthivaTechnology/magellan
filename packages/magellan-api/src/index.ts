c4668e056624ab6a7725437bd1a15aa6395cfe31
Sasidharan Srinivasan <114056156+sasidhar-dev@users.noreply.github.com>
2023-09-27 16:13:45 +0530

    Add files via upload

--- packages/magellan-api/src/index.ts
+++ packages/magellan-api/src/index.ts
@@ -1,30 +1,41 @@
-import { ApolloServer, gql } from 'apollo-server'
-import { readFileSync } from 'fs'
-import { DocumentNode } from 'graphql'
-import { createContext } from './createContext'
-import { resolvers } from './resolvers'
-import express from 'express'
+import { ApolloServer, gql } from 'apollo-server-express';
+import { readFileSync } from 'fs';
+import { DocumentNode } from 'graphql';
+import { createContext } from './createContext';
+import { resolvers } from './resolvers';
+import express from 'express';
 
-const server = new ApolloServer({
-  typeDefs: loadSchema(),
-  resolvers,
-  context: createContext(),
-  csrfPrevention: true,
-  cache: 'bounded'
-})
+process.env.AWS_PROFILE = '301838289846_AdministratorAccess';
+process.env.AWS_REGION = 'ap-south-1';
 
-const app = express()
+async function startApolloServer() {
+  const server = new ApolloServer({
+    typeDefs: loadSchema(),
+    resolvers,
+    context: createContext(),
+    csrfPrevention: true,
+    cache: 'bounded'
+  });
 
-server.applyMiddleware()
+  await server.start();
 
-app.get('/health', (req, res) => {
-  res.status(200).send('Okay!')
-})
+  const app = express();
 
-server.listen().then(({ url }) => {
-  console.log(`🚀  Server ready at ${url}`)
-})
+  server.applyMiddleware({ app });
+
+  app.get('/health', (req: any, res: any) => {
+    res.status(200).send('Okay!');
+  });
+
+  app.listen({ port: 4000 }, () => {
+    console.log('🚀  Server ready at http://localhost:4000/graphql');
+  });
+}
 
 function loadSchema(): DocumentNode {
-  return gql(readFileSync(`${__dirname}/schema.graphql`, { encoding: 'utf-8' }))
+  return gql(readFileSync(`${__dirname}/schema.graphql`, { encoding: 'utf-8' }));
 }
+
+startApolloServer().catch((error) => {
+  console.error('Error starting Apollo Server:', error);
+});
